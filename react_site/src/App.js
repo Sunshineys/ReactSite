3d378532b18a5bf6fb98cdd0e71cca3aea2b0c21
Sunshineys <dream.sunshine88@gmail.com>
2022-02-11 16:13:38 +0200

    first try

--- react_site/src/App.js
+++ react_site/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.sass';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="logo">
+          <div className="logo_text">
+            <h1>КиноМонстр</h1>
+            <h2>Кино - наша страсть!</h2>
+          </div>
+
+        </div>
       </header>
     </div>
   );
